6392968e28c1c10b496bc026f5c5f214f652526c
crownedpix <crownedpix@gmail.com>
2020-08-17 14:48:11 +0530

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,28 +11,58 @@ export class AppComponent {
   subscription: Subscription;
   gridx = "10";
   gridy = "10";
+  steps = 0;
   playerx = 0;
   playery = 0;
   setGridx = [];
   setGridy = [];
   randomnum = [];
   ngAfterContentInit() {
+    
     this.taketheGrids();
+    
+  }
+  ngOnChange(){
+    if(this.randomnum.length == 0){
+      alert("You completed with "+ this.steps + "steps.")
+    }
   }
   ngOnInit() {
+    console.log("init");
     this.subscription = fromEvent(document, 'keypress').subscribe(e => {
       // w,a,s,d keys working on the dashboard
+      console.log(e['key']);
       if(e['key'] === 'w'){
-        console.log("up");
+        this.playerx--;
       }
       if(e['key'] === 'a'){
-        console.log("left");
+        this.playery--;
       }
       if(e['key'] === 's'){
-        console.log("down");
+        this.playerx++;
       }
       if(e['key'] === 'd'){
-        console.log("right");
+        this.playery++;
+      }
+      if(this.playerx > Number(this.gridx)){
+        this.playerx = 1;
+      }
+      if(this.playery > Number(this.gridy)){
+        this.playery = 1;
+      }
+      if(this.playerx < 1){
+        this.playerx = Number(this.gridx);
+      }
+      if(this.playery < 1){
+        this.playery = Number(this.gridy);
+      }
+      let c = Number(this.playerx+''+this.playery);
+      this.steps++;
+      if(this.randomnum.includes(c)){
+        this.randomnum.splice(this.randomnum.indexOf(c),1);
+        setTimeout(()=>{if(this.randomnum.length == 0){
+          alert("You completed with "+ this.steps + "steps.")
+        }},100);
       }
     })
   }
@@ -53,18 +83,28 @@ export class AppComponent {
     } else {
       a = Number(this.gridy);
     }
-    // setting an array of random numbers to display
-    while(this.randomnum.length != a) {
-      this.randomnum.filter((x, i, a) => a.indexOf(x) == i);
+    // create random player placement
+    this.playerx = Math.floor(Math.random() * Number(this.gridx)+1);
+    this.playery = Math.floor(Math.random() * Number(this.gridy)+1);
+    // push to random array store
+    this.randomnum.push(Number(this.playerx+''+this.playery));
+    // setting an unique array of random numbers along with player data to display
+    while(this.randomnum.length != a+1) {
+      if(this.randomnum.length > 0)
+      this.randomnum = this.randomnum.filter((x, i, a) => a.indexOf(x) === i);
+      console.log(this.randomnum);
       this.randomnum.push(Number((Math.floor(Math.random() * Number(this.gridx)) + 1) + '' + (Math.floor(Math.random() * Number(this.gridy)) + 1 )));
     }
-    this.playerx = Math.floor(Math.random() * Number(this.gridx)) + 1;
-    this.playery = Math.floor(Math.random() * Number(this.gridy)) + 1;
+    // remove player data
+    this.randomnum.shift();
+    console.log("contnet");
   }
 
   setRandomBlob(x, y) {
+    console.log("random");
     // iterating and setting the random objects along with green hero
     let c = Number(x+''+y);
+
     // for enemies
     if (this.randomnum.includes(c) && c !== Number(this.playerx+''+this.playery)) {
       return {
@@ -78,6 +118,9 @@ export class AppComponent {
       }
     }
     
+    
   }
+
+  
 
 }
